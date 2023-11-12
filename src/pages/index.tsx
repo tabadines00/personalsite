ac2b4b9f91e7fdeb7ee2b148aeb3715a87903c40
tabadines00 <thomas.abadines@gmail.com>
2023-11-12 15:02:09 -0800

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,31 @@ import * as React from 'react'
 import type { HeadFC, PageProps } from "gatsby"
 
 const Index: React.FC<PageProps> = () => {
+
+	const [message, setMessage] = React.useState("default")
+
+  	const url = "https://lj4dsvnwho2gufukhy3s7qxduq0wqerx.lambda-url.us-west-1.on.aws/";
+	const options = {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json;charset=UTF-8",
+		},
+	}
+
+	fetch(url, options)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			setMessage(data);
+	})
+
   return (
-    <main className="grid place-items-center h-screen">
-      Gatsby is cool!
-    </main>
+	<main className="grid place-items-center h-screen">
+	  Gatsby is cool!
+	  Also Hi Thomas!
+	  {message}
+	</main>
   )
 }
 
